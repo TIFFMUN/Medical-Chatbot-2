bb2ed481905d76bcb88a8024e27d8869cb721569
TIFFMUN <tiffanymun1@gmail.com>
2024-08-11 14:27:09 +0800

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot 2',
+    version= '0.0.0',
+    author= 'Tiffany Mun',
+    author_email= 'tiffanymun1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
